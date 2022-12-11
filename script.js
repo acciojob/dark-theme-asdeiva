3b9056fb78ac116b8cd6ddb355dbd761790882ac
DEIVA SIVAM <a.s.deiva@gmail.com>
2022-12-11 11:47:40 +0530

    script updated

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-//your code here
+var button = document.querySelector("button");
+var container = document.querySelector("main");
 
+function swap(){
+
+    if (container.getAttribute("class") != "night") {
+        container.setAttribute("class", "night");
+        button.setAttribute("class","button_night")
+      }
+      else {
+        container.setAttribute("class", "day");
+        button.setAttribute("class","button_day")
+  }
+}
+
+button.onclick = swap;
